2ede58382a13e026878c89f0972944dc79984e7b
Md. Mehedi Hasan <mehedi15-9021@diu.edu.bd>
2025-07-29 01:27:24 +0600

    first commit

---
+++ src/main/java/com/mocion/web/pages/PublicEventPage.java
@@ -0,0 +1,206 @@
+package com.mocion.web.pages;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+
+import java.nio.file.Paths;
+
+public class PublicEventPage {
+    private final Page page;
+
+    public String eventsText = "text='Events'";
+    public String clubNameDropdown = "select[title='Club name here']";
+    public String createButton = "role=button[name='Create']";
+    public String publicEventText = "text='Public event'";
+    public String nameInputField = "input[name='name']";
+    public String organizationNameField = "input[name='organizer_name']";
+    public String eventDescriptionField = "input[name='description']";
+    public String sponsorField = "input[name='sponsor_ids.0.name']";
+    public String sponsorLogoField = "label[for='clubLogo']";
+    public String prizeField = "input[name='prize_ids.0.name']";
+    public String eventImageField = "input#trn_image_upload";
+    public String eventCreateDropdowns = ".react-select__dropdown-indicator";
+    public String minPlayerLevelField = "input[name='min_players_level']";
+    public String maxPlayerLevelField = "input[name='max_players_level']";
+    public String termsAndConditionsField = "input[name='terms_and_conditions']";
+    public String pricePerPlayerField = "input[name='price_per_player']";
+    public String genderMixed = "input[type='radio'][value='Mixed']";
+    public String eventTypeCompetitive = "input[type='radio'][value='competitive']";
+    public String dateFields = "div.absolute.right-1.top-2 svg";
+    public String startTime = "input[name='start_time']";
+    public String endTime = "input[name='end_time']";
+    public String paymentDetailsApp = "text='App'";
+    public String courtTypeIndoor = "text='Indoor'";
+    public String eventStyleAmericano = "input[type='radio'][value='Americano']";
+    public String eventStyleMexicano = "input[type='radio'][value='Mexicano']";
+    public String playerJoiningTypeSingle = "input[type='radio'][value='Single']";
+    public String playerJoiningTypeBoth = "input[type='radio'][value='Both']";
+    public String saveEventButton = "button.bg-primary.text-white";
+    public String eventCreateSuccessMessage = "text='Successful.'";
+    public String minNumberOfPlayers = "input[name='min_number_of_players']";
+    public String maxNumberOfPlayers = "input[name='max_number_of_players']";
+
+    public PublicEventPage(Page page) {
+        this.page = page;
+    }
+
+    public PublicEventPage clickEventsFromNavigationBar() {
+        page.locator(eventsText).click();
+        return this;
+    }
+
+    public PublicEventPage clickPublicEventFromNavigationBar() {
+        page.locator(publicEventText).click();
+        return this;
+    }
+
+    public PublicEventPage selectClubName(String clubName) {
+        page.selectOption(clubNameDropdown, clubName);
+        return this;
+    }
+
+    public PublicEventPage clickCreateButton() {
+        page.locator(createButton).click();
+        return this;
+    }
+
+    public PublicEventPage fillEventName(String eventName) {
+        page.locator(nameInputField).fill(eventName);
+        return this;
+    }
+
+    public PublicEventPage fillOrganizationName(String organizationName) {
+        page.locator(organizationNameField).fill(organizationName);
+        return this;
+    }
+
+    public PublicEventPage fillEventDescription(String eventDescription) {
+        page.locator(eventDescriptionField).fill(eventDescription);
+        return this;
+    }
+
+    public PublicEventPage fillSponsor(String sponsor) {
+        page.locator(sponsorField).fill(sponsor);
+        return this;
+    }
+
+    public PublicEventPage uploadSponsorLogo() {
+        Locator sponsorLogo = page.locator(sponsorLogoField);
+        sponsorLogo.setInputFiles(Paths.get("src/main/resources/club_image.png"));
+        return this;
+    }
+
+    public PublicEventPage fillPrize(String prize) {
+        page.locator(prizeField).fill(prize);
+        return this;
+    }
+
+    public PublicEventPage uploadEventImage() {
+        Locator eventImage = page.locator(eventImageField);
+        eventImage.setInputFiles(Paths.get("src/main/resources/club_image.png"));
+        return this;
+    }
+
+    public PublicEventPage fillMinNumberOfPlayers(String minPlayerLevel) {
+        page.locator(minNumberOfPlayers).fill(minPlayerLevel);
+        return this;
+    }
+
+    public PublicEventPage fillMaxNumberOfPlayers(String minPlayerLevel) {
+        page.locator(maxNumberOfPlayers).fill(minPlayerLevel);
+        return this;
+    }
+
+    public PublicEventPage fillMinPlayerLevelField(String minPlayerLevel) {
+        page.locator(minPlayerLevelField).fill(minPlayerLevel);
+        return this;
+    }
+
+    public PublicEventPage fillMaxPlayerLevel(String maxPlayerLevel) {
+        page.locator(maxPlayerLevelField).fill(maxPlayerLevel);
+        return this;
+    }
+
+    public PublicEventPage fillPricePerPlayer(String pricePerPlayer) {
+        page.locator(pricePerPlayerField).fill(pricePerPlayer);
+        return this;
+    }
+
+    public PublicEventPage selectPaymentDetailsApp() {
+        page.locator(eventCreateDropdowns).nth(0).click();
+        page.locator(paymentDetailsApp).click();
+        return this;
+    }
+
+    public PublicEventPage selectGenderMixed() {
+        page.locator(genderMixed).click();
+        return this;
+    }
+
+    public PublicEventPage selectAssignOrganizer() {
+        page.locator(eventCreateDropdowns).nth(1).click();
+        page.keyboard().press("Enter");
+        return this;
+    }
+
+    public PublicEventPage fillTermsAndConditions(String termsAndConditions) {
+        page.locator(termsAndConditionsField).fill(termsAndConditions);
+        return this;
+    }
+
+    public PublicEventPage selectStartDate() {
+        page.locator(dateFields).nth(0).click();
+        page.keyboard().press("Enter");
+        return this;
+    }
+
+    public PublicEventPage selectRegistrationDeadline() {
+        page.locator(dateFields).nth(1).click();
+        page.keyboard().press("Enter");
+        return this;
+    }
+
+    public PublicEventPage selectEventStyleAmericano() {
+        page.locator(eventStyleAmericano).click();
+        return this;
+    }
+
+    public PublicEventPage selectEventStyleMexicano() {
+        page.locator(eventStyleMexicano).click();
+        return this;
+    }
+
+    public PublicEventPage selectEventTypeCompetitive() {
+        page.locator(eventTypeCompetitive).click();
+        return this;
+    }
+
+    public PublicEventPage selectPlayerJoiningTypeSingle() {
+        page.locator(playerJoiningTypeSingle).click();
+        return this;
+    }
+
+    public PublicEventPage selectStartTime() {
+        page.locator(startTime).fill("06:00");
+        return this;
+    }
+
+    public PublicEventPage selectEndTime() {
+        page.locator(endTime).fill("23:00");
+        return this;
+    }
+
+    public PublicEventPage selectCourtTypeIndoor() {
+        page.locator(eventCreateDropdowns).nth(2).click();
+        page.locator(courtTypeIndoor).nth(1).click();
+        return this;
+    }
+
+    public void clickSaveEventButton() {
+        page.locator(saveEventButton).click();
+    }
+
+    public Locator eventtCreateSuccessMessageLocator() {
+        return page.locator(eventCreateSuccessMessage);
+    }
+}
---
+++ src/test/java/com/mocion/test/PublicEventTest.java
@@ -0,0 +1,128 @@
+package com.mocion.test;
+
+import com.mocion.web.pages.LocationPage;
+import com.mocion.web.pages.LoginPage;
+import com.mocion.web.pages.PublicEventPage;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class PublicEventTest extends BaseTest {
+    public LoginPage loginPage;
+    public PublicEventPage publicEventPage;
+    public Random random;
+    public LocationPage locationPage;
+
+    private static final String CLUB_NAME = "Farah123";
+    private static final String MIN_PLAYER_LEVEL = "0";
+    private static final String MAX_PLAYER_LEVEL = "5";
+    private static final String MIN_NUMBER_OF_PLAYERS = "8";
+    private static final String MAX_NUMBER_OF_PLAYERS = "10";
+    private static final String PRICE_PER_PLAYER = "10";
+
+    public void initPages() {
+        random = new Random();
+        loginPage = new LoginPage(page);
+        publicEventPage = new PublicEventPage(page);
+        locationPage = new LocationPage(page);
+    }
+
+    @Test(description = "Public competitive americano event create with joining type single should successful")
+    public void verify_public_competitive_americano_event_create_with_joining_type_single_should_succeed() {
+        List<String> data = generateEventData();
+
+        initPages();
+        locationPage.setLocationPermissionAllowed();
+        loginPage.userLogin();
+        publicEventPage
+                .clickEventsFromNavigationBar()
+                .clickPublicEventFromNavigationBar()
+                .selectClubName(CLUB_NAME)
+                .clickCreateButton()
+                .fillEventName(data.get(0))
+                .fillOrganizationName(data.get(1))
+                .fillEventDescription(data.get(4))
+                .fillSponsor(data.get(2))
+                .uploadSponsorLogo()
+                .fillPrize(data.get(3))
+                .uploadEventImage()
+                .fillMinNumberOfPlayers(MIN_NUMBER_OF_PLAYERS)
+                .fillMaxNumberOfPlayers(MAX_NUMBER_OF_PLAYERS)
+                .fillMinPlayerLevelField(MIN_PLAYER_LEVEL)
+                .fillMaxPlayerLevel(MAX_PLAYER_LEVEL)
+                .fillPricePerPlayer(PRICE_PER_PLAYER)
+                .selectPaymentDetailsApp()
+                .selectEventStyleAmericano()
+                .selectEventTypeCompetitive()
+                .selectPlayerJoiningTypeSingle()
+                .selectGenderMixed()
+                .selectAssignOrganizer()
+                .fillTermsAndConditions(data.get(5))
+                .selectStartDate()
+                .selectRegistrationDeadline()
+                .selectCourtTypeIndoor()
+                .selectStartTime()
+                .selectEndTime()
+                .clickSaveEventButton();
+
+        assertThat(publicEventPage.eventtCreateSuccessMessageLocator()).isVisible();
+    }
+
+    @Test(description = "Public competitive mexicano event create with joining type single should successful")
+    public void verify_public_competitive_mexicano_event_create_with_joining_type_single_should_succeed() {
+        List<String> data = generateEventData();
+
+        initPages();
+        locationPage.setLocationPermissionAllowed();
+        loginPage.userLogin();
+        publicEventPage
+                .clickEventsFromNavigationBar()
+                .clickPublicEventFromNavigationBar()
+                .selectClubName(CLUB_NAME)
+                .clickCreateButton()
+                .fillEventName(data.get(0))
+                .fillOrganizationName(data.get(1))
+                .fillEventDescription(data.get(4))
+                .fillSponsor(data.get(2))
+                .uploadSponsorLogo()
+                .fillPrize(data.get(3))
+                .uploadEventImage()
+                .fillMinNumberOfPlayers(MIN_NUMBER_OF_PLAYERS)
+                .fillMaxNumberOfPlayers(MAX_NUMBER_OF_PLAYERS)
+                .fillMinPlayerLevelField(MIN_PLAYER_LEVEL)
+                .fillMaxPlayerLevel(MAX_PLAYER_LEVEL)
+                .fillPricePerPlayer(PRICE_PER_PLAYER)
+                .selectPaymentDetailsApp()
+                .selectEventStyleMexicano()
+                .selectEventTypeCompetitive()
+                .selectPlayerJoiningTypeSingle()
+                .selectGenderMixed()
+                .selectAssignOrganizer()
+                .fillTermsAndConditions(data.get(5))
+                .selectStartDate()
+                .selectRegistrationDeadline()
+                .selectCourtTypeIndoor()
+                .selectStartTime()
+                .selectEndTime()
+                .clickSaveEventButton();
+
+        assertThat(publicEventPage.eventtCreateSuccessMessageLocator()).isVisible();
+    }
+
+    private List<String> generateEventData() {
+        random = new Random();
+        String id = String.format("%03d", random.nextInt(999) + 1);
+        return Arrays.asList(
+                "test_event_" + id,
+                "test_organization_" + id,
+                "test_sponsor_" + id,
+                "test_prize_" + id,
+                "test_description",
+                "test_terms_and_conditions"
+        );
+    }
+}

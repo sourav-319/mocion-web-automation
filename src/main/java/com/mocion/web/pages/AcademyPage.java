db7373982ae942120e6760221784e2de7de755b8
Md. Mehedi Hasan <mehedi15-9021@diu.edu.bd>
2025-08-12 00:45:56 +0600

    first commit

--- src/main/java/com/mocion/web/pages/AcademyPage.java
+++ src/main/java/com/mocion/web/pages/AcademyPage.java
@@ -1,5 +1,6 @@
 package com.mocion.web.pages;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 
 import java.util.Arrays;
@@ -21,8 +22,22 @@ public class AcademyPage {
     public String maximumNumberOfPlayersField = "input[name='max_number_of_players']";
     public String bookingInAppOn = "input[type='radio'][value='true']";
     public String dateFields = "div.absolute.right-1.top-2 svg";
-    public String startTime = "input[name='start_time']";
-    public String endTime = "input[name='end_time']";
+    public String startTimeField = "#react-select-6-input";
+    public String endTimeField = ".react-select__value-container";
+    public String checkAvailableButton = "button:has-text('check available')";
+    public String availableCourt = ".p-2.ml-2.px-4.rounded-lg";
+    public String saveAndPublishButton = "button:has-text('save and publish')";
+    public String academyCreateSuccessMessage = "text='Successful.'";
+    public String coachFieldDropdown = "div.react-select.__indicator";
+    public String startTime = "text='12:00 PM'";
+    public String endTime = "text='4:00 PM'";
+    public String menuIcon = "button:has(svg.w-full.h-auto)";
+    public String addPlayerText = "text='Add Players'";
+    public String addPlayersDropdown = "div.react-select.__indicator.__dropdown-indicator";
+    public String PaymentMethodDropdown = "div.react-select__indicators div.react-select__indicator";
+    public String paymentMethodInApp = "text='In app'";
+    public String saveButton = "button:has-text('Save')";
+    public String addPlayersToAcademySuccessMessage = "text='players were added successfully'";
 
     public AcademyPage(Page page) {
         this.page = page;
@@ -66,7 +81,7 @@ public class AcademyPage {
     }
 
     public AcademyPage selectCoach() {
-        page.locator(academyCreateDropdowns).nth(2).click();
+        page.locator(coachFieldDropdown).click();
         page.keyboard().press("Enter");
         return this;
     }
@@ -109,7 +124,7 @@ public class AcademyPage {
                 "Friday", "Saturday", "Sunday"
         );
 
-        page.locator(academyCreateDropdowns).nth(3).click();
+        page.locator(academyCreateDropdowns).nth(2).click();
 
         for (String day : daysToSelect) {
             int index = allDays.indexOf(day);
@@ -127,12 +142,63 @@ public class AcademyPage {
     }
 
     public AcademyPage selectStartTime() {
-        page.locator(startTime).fill("06:00");
+        page.locator(startTimeField).click();
+        page.locator(startTime).last().click();
         return this;
     }
 
-    public AcademyPage selectEndTime() {
-        page.locator(endTime).fill("22:00");
+    public AcademyPage selectEndTime() throws InterruptedException {
+        Thread.sleep(2000);
+        page.locator(endTimeField).last().click();
+        page.locator(endTime).click();
         return this;
+    }
+
+    public AcademyPage clickCheckAvailableButton() {
+        page.locator(checkAvailableButton).click();
+        return this;
+    }
+
+    public AcademyPage selectAvailableCourt() {
+        page.locator(availableCourt).nth(5).click();
+        return this;
+    }
+
+    public AcademyPage clickMenuIcon() {
+        page.locator(menuIcon).nth(3).click();
+        return this;
+    }
+
+    public AcademyPage clickAddPlayers() {
+        page.locator(addPlayerText).click();
+        return this;
+    }
+
+    public AcademyPage selectPlayerNameToAdd() {
+        page.locator(addPlayersDropdown).nth(1).click();
+        page.keyboard().press("Enter");
+        return this;
+    }
+
+    public AcademyPage selectPaymentMethodInApp() {
+        page.locator(PaymentMethodDropdown).nth(1).click();
+        page.locator(paymentMethodInApp).click();
+        return this;
+    }
+
+    public void clickSaveButton() {
+        page.locator(saveButton).click();
+    }
+
+    public void clickSaveAndPublishButton() {
+        page.locator(saveAndPublishButton).click();
+    }
+
+    public Locator academyCreateSuccessMessageLocator() {
+        return page.locator(academyCreateSuccessMessage);
+    }
+
+    public Locator addPlayerToAcademySuccessMessageLocator() {
+        return page.locator(addPlayersToAcademySuccessMessage);
     }
 }
--- src/test/java/com/mocion/test/AcademyTest.java
+++ src/test/java/com/mocion/test/AcademyTest.java
@@ -1,4 +1,94 @@
 package com.mocion.test;
 
-public class AcademyTest {
+import com.mocion.web.pages.AcademyPage;
+import com.mocion.web.pages.LocationPage;
+import com.mocion.web.pages.LoginPage;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class AcademyTest extends BaseTest {
+    public LoginPage loginPage;
+    public Random random;
+    public LocationPage locationPage;
+    public AcademyPage academyPage;
+
+    private static final String CLUB_NAME = "Farah123";
+    private static final String REGISTRATION_PRICE_PER_PLAYER = "10";
+    private static final String max_number_OF_PLAYERS = "10";
+    private static final List<String> ALLOWED_DAYS = Arrays.asList("Monday", "Tuesday", "Wednesday", "Thursday", "Friday");
+
+    public void initPages() {
+        random = new Random();
+        loginPage = new LoginPage(page);
+        academyPage = new AcademyPage(page);
+        locationPage = new LocationPage(page);
+    }
+
+    @Test(description = "Public indoor academy create with booking in app should successful")
+    public void verify_public_indoor_academy_create_with_booking_in_app_should_succeed() throws InterruptedException {
+        List<String> data = generateAcademyData();
+
+        initPages();
+        locationPage.setLocationPermissionAllowed();
+        loginPage.userLogin();
+        academyPage
+                .clickCoachingFromNavigationBar()
+                .clickAcademyEventFromNavigationBar()
+                .selectClubName(CLUB_NAME)
+                .clickCreateButton()
+                .fillAcademyName(data.getFirst())
+                .selectCourtTypeIndoor()
+                .selectCourtPrivacyPublic()
+                .selectCoach()
+                .fillRegistrationPricePerPlayer(REGISTRATION_PRICE_PER_PLAYER)
+                .fillMaximumNumberOfPlayers(max_number_OF_PLAYERS)
+                .selectGenderBoth()
+                .selectBookingInAppOn()
+                .selectStartDate()
+                .selectEndDate()
+                .selectAllowedDays(ALLOWED_DAYS)
+                .selectEndTime()
+                .selectStartTime()
+                .clickCheckAvailableButton()
+                .selectAvailableCourt()
+                .clickSaveAndPublishButton();
+
+        assertThat(academyPage.academyCreateSuccessMessageLocator()).isVisible();
+    }
+
+    @Test(description = "Add players to academy with payment in app should successful")
+    public void verify_add_player_to_academy_with_payment_in_app_should_succeed() throws InterruptedException {
+        initPages();
+        locationPage.setLocationPermissionAllowed();
+        loginPage.userLogin();
+        academyPage
+                .clickCoachingFromNavigationBar()
+                .clickAcademyEventFromNavigationBar()
+                .selectClubName(CLUB_NAME)
+                .clickMenuIcon()
+                .clickAddPlayers()
+                .selectPlayerNameToAdd()
+                .selectPaymentMethodInApp()
+                .clickSaveButton();
+
+        assertThat(academyPage.addPlayerToAcademySuccessMessageLocator()).isVisible();
+    }
+
+    private List<String> generateAcademyData() {
+        random = new Random();
+        String id = String.format("%03d", random.nextInt(999) + 1);
+        return Arrays.asList(
+                "test_academy_" + id,
+                "test_organization_" + id,
+                "test_sponsor_" + id,
+                "test_prize_" + id,
+                "test_description",
+                "test_terms_and_conditions"
+        );
+    }
 }
